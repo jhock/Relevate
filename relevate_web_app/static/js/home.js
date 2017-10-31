2280623f1cdaf4962ba13a761fcebcd1578e2f2d
sjensen90 <sjense90@gmail.com>
2017-10-30 20:25:29 -0500

    Small clean up and organization changes for homepage
    - Removal of the cloud background for less clutter
      and improved clarity
    - Changed card background to white for a cleaner feel
    - Removal of the "user is not logged in" prompt
      that is stuck at the top of the page. Instead
      the relevate info is displayed when the user is not
      authenticated
    - If the user is authenticated, it displays a welcome
      for that particular user and prompts for confirmation
      if they have not confirmed their account

--- relevate_web_app/static/js/home.js
+++ relevate_web_app/static/js/home.js
@@ -1,11 +1,18 @@
 $(function(){
+
+  // We're going to go with a white background on the cards
+  // for a cleaner feel. But we'll change the heading colors
+  // (possibly random as well) so we'll need this js moving 
+  // forward. We'll just repurpose it to apply to the card
+  // attributes we want to colorize later on. I've commented
+  // it out for now.
   
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-  var colors = ['#b7d3f2', '#afafdc', '#8a84e2', '#84afe6', '#79beee'];
-  $('.item').each(function(index,el){
-    var randomColorIndex = getRandomInt(0,colors.length-1);
-    $(el).css('background-color',colors[randomColorIndex]);
-  });
+  // function getRandomInt(min, max) {
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // };
+  // var colors = ['#b7d3f2', '#afafdc', '#8a84e2', '#84afe6', '#79beee'];
+  // $('.item').each(function(index,el){
+  //   var randomColorIndex = getRandomInt(0,colors.length-1);
+  //   $(el).css('background-color',colors[randomColorIndex]);
+  // });
 });

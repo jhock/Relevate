d3627407d0ff6b797fd145159075699eb5e0ba00
sjensen90 <sjense90@gmail.com>
2018-10-07 11:25:17 -0500

    A tab list component
    Test Plan:
    - Navigate to the styleguide
    - Observe that there is now a tab list component
    - Selecting tabs via click should change the tabs
    - The selected tab should have tabindex 0, the non
      selected tabs should have tabindex -1
    - Arrow keys should navigate between the tabs
    - Screen readers should announce tab and tabpanel roles

--- relevate_web_app/apps/components/templatetags/tab_list_tags.py
+++ relevate_web_app/apps/components/templatetags/tab_list_tags.py
@@ -8,6 +8,8 @@ from ..utils import (
   get_prop_value,
   resolve_variable,
 )
+import uuid
+import pdb
 
 register = template.Library()
 
@@ -26,19 +28,38 @@ def tab_list(parser, token):
 def tab_panel(parser, token):
   children, _ = parse_tag(parser, token, 'end_tab_panel')
   props = token.split_contents()[1:]
+
   enforce_required_props(['title'], props)
   title = get_prop_value('title', props, None)
-  return TabPanel(children, title)
+
+  return TabPanel(children, title, str(uuid.uuid4().int)[:10])
 
 class TabList(Node):
   def __init__(self, tab_panels):
     self.tab_panels = tab_panels
 
   def render(self, context):
-    markup = ''
+    tabs = []
+    panel_markup = ''
     for i in range(0, len(self.tab_panels)):
       tab_panel = self.tab_panels[i]
-      markup = markup + tab_panel.render(context)
+      tab_id = str(uuid.uuid4().int)[:10]
+      tabs.append(Tab(tab_panel.title, tab_id, tab_panel.panel_id))
+      context.update({
+        'active': i == 0,
+        'tab_id': tab_id
+      })
+      panel_markup = panel_markup + tab_panel.render(context)
+
+    tab_markup = ''
+    for i in range(0, len(tabs)):
+      tab = tabs[i]
+      context.update({
+        'active': i == 0
+      })
+      tab_markup = tab_markup + tab.render(context)
+
+    markup = tab_markup + panel_markup
 
     context.update({
       'tab_panels': format_html(markup)
@@ -47,17 +68,35 @@ class TabList(Node):
     tab_list_html = get_template('tab_list/index.html')
     return tab_list_html.render(context)
 
+class Tab(Node):
+  def __init__(self, title, tab_id, panel_id):
+    self.title = title
+    self.tab_id = tab_id
+    self.panel_id = panel_id
+
+  def render(self, context):
+    tab_html = get_template('tab_list/tab/index.html')
+
+    context.update({
+      'title': resolve_variable(self.title, context),
+      'tab_id': self.tab_id,
+      'panel_id': self.panel_id
+    })
+
+    return tab_html.render(context)
+
 class TabPanel(Node):
-  def __init__(self, children, title):
+  def __init__(self, children, title, panel_id):
     self.children = children
     self.title = title
+    self.panel_id = panel_id
 
   def render(self, context):
     tab_panel_html = get_template('tab_list/tab_panel/index.html')
 
     context.update({
       'children': self.children.render(context),
-      'title': resolve_variable(self.title, context)
+      'panel_id': self.panel_id
     })
 
     return tab_panel_html.render(context)

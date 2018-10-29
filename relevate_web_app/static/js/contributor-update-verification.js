867adb53ca2ceffa02cef13dd283610a0115574b
jhock <joshua.a.hock@gmail.com>
2018-10-28 20:21:59 -0500

    Added cropping function to 'Avatar' form field on page 'Contributor Update'

--- relevate_web_app/static/js/contributor-update-verification.js
+++ relevate_web_app/static/js/contributor-update-verification.js
@@ -7,7 +7,6 @@
 $(document).ready(function()
 {
     var currentTab = 0; // Current tab is set to be the first tab (0)
-    showTab(currentTab); // Display the current tab
 
     console.log("test next button function");
 	verificationFlags = {
@@ -175,61 +174,63 @@ $(document).ready(function()
 		}
 	});
 
-		 /* SCRIPT TO OPEN THE MODAL WITH THE PREVIEW */
-      $("#id_avatar").change(function () {
-        if (this.files && this.files[0]) {
-          var reader = new FileReader();
-          reader.onload = function (e) {
-            $("#image").attr("src", e.target.result);
-            $("#modalCrop").modal("show");
-          }
-          reader.readAsDataURL(this.files[0]);
-        }
-      });
+     /* SCRIPT TO OPEN THE MODAL WITH THE PREVIEW */
+    $("#id_avatar").change(function () {
+    console.log('test');
+    if (this.files && this.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+      console.log('test');
+        $("#image").attr("src", e.target.result);
+        $("#modalCrop").modal("show");
+      }
+      reader.readAsDataURL(this.files[0]);
+    }
+    });
 
-      /* SCRIPTS TO HANDLE THE CROPPER BOX */
-      var $image = $("#image");
-      var cropBoxData;
-      var canvasData;
-      $("#modalCrop").on("shown.bs.modal", function () {
-        $image.cropper({
-          viewMode: 1,
-          aspectRatio: 1/1,
-          minCropBoxWidth: 200,
-          minCropBoxHeight: 200,
-          ready: function () {
-            $image.cropper("setCanvasData", canvasData);
-            $image.cropper("setCropBoxData", cropBoxData);
-          }
-        });
-      }).on("hidden.bs.modal", function () {
-        cropBoxData = $image.cropper("getCropBoxData");
-        canvasData = $image.cropper("getCanvasData");
-        var cropData = $image.cropper("getData");
-        $("#id_x").val(cropData["x"]);
-        $("#id_y").val(cropData["y"]);
-        $("#id_height").val(cropData["height"]);
-        $("#id_width").val(cropData["width"]);
-        $image.cropper("destroy");
-      });
+    /* SCRIPTS TO HANDLE THE CROPPER BOX */
+    var $image = $("#image");
+    var cropBoxData;
+    var canvasData;
+    $("#modalCrop").on("shown.bs.modal", function () {
+    $image.cropper({
+      viewMode: 1,
+      aspectRatio: 1/1,
+      minCropBoxWidth: 200,
+      minCropBoxHeight: 200,
+      ready: function () {
+        $image.cropper("setCanvasData", canvasData);
+        $image.cropper("setCropBoxData", cropBoxData);
+      }
+    });
+    }).on("hidden.bs.modal", function () {
+    cropBoxData = $image.cropper("getCropBoxData");
+    canvasData = $image.cropper("getCanvasData");
+    var cropData = $image.cropper("getData");
+    $("#id_x").val(cropData["x"]);
+    $("#id_y").val(cropData["y"]);
+    $("#id_height").val(cropData["height"]);
+    $("#id_width").val(cropData["width"]);
+    $image.cropper("destroy");
+    });
 
-      $(".js-zoom-in").click(function () {
-        $image.cropper("zoom", 0.1);
-      });
+    $(".js-zoom-in").click(function () {
+    $image.cropper("zoom", 0.1);
+    });
 
-      $(".js-zoom-out").click(function () {
-        $image.cropper("zoom", -0.1);
-      });
+    $(".js-zoom-out").click(function () {
+    $image.cropper("zoom", -0.1);
+    });
 
-      /* SCRIPT TO COLLECT THE DATA AND POST TO THE SERVER */
-      $(".js-crop-and-upload").click(function () {
-        var cropData = $image.cropper("getData");
-        $("#id_x").val(cropData["x"]);
-        $("#id_y").val(cropData["y"]);
-        $("#id_height").val(cropData["height"]);
-        $("#id_width").val(cropData["width"]);
-        $("#modalCrop").modal("hide");
-      });
+    /* SCRIPT TO COLLECT THE DATA AND POST TO THE SERVER */
+    $(".js-crop-and-upload").click(function () {
+    var cropData = $image.cropper("getData");
+    $("#id_x").val(cropData["x"]);
+    $("#id_y").val(cropData["y"]);
+    $("#id_height").val(cropData["height"]);
+    $("#id_width").val(cropData["width"]);
+    $("#modalCrop").modal("hide");
+    });
 
 
     function showTab(n) {
